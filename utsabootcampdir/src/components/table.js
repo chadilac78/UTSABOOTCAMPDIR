378b910e1c449a2cb01015c9d774142238ab3c79
orthCWawn <chadilac7894@gmail.com>
2020-06-07 14:36:25 -0500

    added search field

--- utsabootcampdir/src/components/table.js
+++ utsabootcampdir/src/components/table.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn, SearchField } from 'react-bootstrap-table';
 import employees from "./employees.json";
 
+// const search = {
+//     SearchField: this.createCustomSearchField
+// };
 
-export default class SortTable extends React.Component {
+
+export default class DefaultCustomSearchFieldTable extends React.Component {
+
+    createCustomSearchField = (props) => {
+        return (
+            <SearchField className='name' defaultValue={props.defualtSearch} placeholder="Search  First Name" />
+        );
+    }
+
 
     state = {
         employees
@@ -11,11 +22,16 @@ export default class SortTable extends React.Component {
 
 
     render() {
+        const options = {
+            clearSearch: true,
+            searchField: this.createCustomSearchField
+        };
         return (
+
             <div>
                 <nav class="navbar navbar-dark bg-dark">
                     <a class="navbar-brand">USTA BOOTCAMP 2020 DIRECTORY</a> </nav>
-                < BootstrapTable data={employees} options={this.options} >
+                < BootstrapTable data={employees} options={options} search>
                     <TableHeaderColumn isKey dataField='id' dataSort={true}>Student ID</TableHeaderColumn>
                     <TableHeaderColumn dataField='name' dataSort={true}>Name</TableHeaderColumn>
                     <TableHeaderColumn dataField='role' dataSort={true}>Role</TableHeaderColumn>
